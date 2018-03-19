1a09e9b7f735e9dcfce1c6ca752677bf12da9364
gregfoulkes <greg_foulkes@live.com>
2018-03-19 15:48:20 +0200

    trying

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,3 +1,36 @@
+var billStringElement = document.querySelector(".billString");
+var calculateBtnElement = document.querySelector(".calculateBtn");
+var billTotalElement = document.querySelector(".billTotal");
+
+calculateBtn.addEventListener('click', calculateBtnClicked(billStringElement){
+
+    // get the string entered in the textArea
+    var billString = billStringField.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+    }
+
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+//return roundBillTotal
+});
+
+
+
+//calculateBtn.addEventListener('click', calculateBtnClicked);
+
 //get a reference to the calculate button
 
 //get a reference to the billTotal element
