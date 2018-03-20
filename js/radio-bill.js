656b1bb9176a6c9d20ad1d0c386ac1616983b84f
gregfoulkes <greg_foulkes@live.com>
2018-03-20 13:00:42 +0200

    add all changes

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -14,3 +14,42 @@
 
 var itemTypeBtn = document.querySelector(".billItemTypeRadio");
 var billAddBtn = document.querySelector(".radioBillAddBtn");
+
+//var billStringField = document.querySelector(".billString");
+//var calculateBtnElement = document.querySelector(".calculateBtn");
+//var billTotalElement = document.querySelector(".billTotal");
+
+
+
+function calculateBtnClicked(){
+    // get the string entered in the textArea
+    var billString = itemTypeBtn.value;
+    //split the string
+    var billItems = billString.split(",");
+    // a variable for the total phone bill.
+    var billTotal = 0;
+    //loop over all the bill items
+    for (var i=0;i<billItems.length;i++){
+        var billItem = billItems[i].trim();
+        if (billItem === "call"){
+            billTotal += 2.75;
+        }
+        else if (billItem === "sms"){
+            billTotal += 0.75;
+        }
+
+        else{
+
+          biilTotal += 0
+        }
+
+
+    }
+
+    //round to two decimals
+    var roundedBillTotal = billTotal.toFixed(2);
+    billTotalElement.innerHTML = roundedBillTotal;
+}
+
+//calculateBtnElement.addEventListener('click', (calculateBtnClicked));
+billAddBtn.addEventListener('click', (calculateBtnClicked));
